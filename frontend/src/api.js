9c124d3e63b94c4d1a91448f7f5ed9681d3390cd
vishal gautam <vishal.psit2213663@gmail.com>
2025-05-22 21:01:31 +0530

    backend deployed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:3000';
+const API_BASE = 'https://todo-summarizer-7xv7.onrender.com';
 
 export async function getTodos() {
   const res = await axios.get(`${API_BASE}/todos`);
